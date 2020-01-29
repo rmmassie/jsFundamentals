7c710cce6c1a8a8cb78c1ada7608d2be3e1b2cd3
Matt Massie <rmmassie@gmail.com>
2020-01-29 15:59:49 -0500

    Day 3 edits

--- 04-Functions/06-return.js
+++ 04-Functions/06-return.js
@@ -8,10 +8,10 @@
 let tip = (bill, tip) => {
     fullAmount = bill * (1 + tip);
     tipAmount = bill * tip;
-    return fullAmount.toFixed(2);
+    return [fullAmount.toFixed(2), tipAmount.toFixed(2)]
 }
 
 let myBill = 45;
 let myTip = .1
 let x = tip(myBill, myTip);
-console.log(`The total bill comes to $${x}.`);
+console.log(`The total bill comes to $${x[0]}. The total tip was ${x[1]}`);
